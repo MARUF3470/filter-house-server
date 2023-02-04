4fb48f81efcc7a5d0d39b7d08cf721a9a47adfd8
MARUF3470 <smmaruf25@gmail.com>
2023-02-04 15:47:55 +0600

    payment method done

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRATE);
+
+app.post("/create-payment-intent", async (req, res) => {
+    const booking = req.body;
+    const price = booking.price;
+    const amount = price * 100
+    const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        "payment_method_types": [
+            "card"
+        ],
+    })
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -18,7 +35,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     const productCollection = client.db('filterHouse').collection('products')
     const cartItemCollection = client.db('filterHouse').collection('cartItems')
+    const paymentItemCollection = client.db('filterHouse').collection('paymentItems')
     const userCollection = client.db('filterHouse').collection('users')
+    const reviewCollection = client.db('filterHouse').collection('reviews')
     try {
         app.post('/products', async (req, res) => {
             const products = req.body
@@ -60,6 +79,34 @@ async function run() {
             const result = await cartItemCollection.insertOne(product)
             res.send(result)
         })
+        app.get('/cartProducts', async (req, res) => {
+            const query = {}
+            const result = await cartItemCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/cartProducts/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const result = await cartItemCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/cartProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await cartItemCollection.findOne(query)
+            res.send(result)
+        })
+        app.delete('/cartProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await cartItemCollection.deleteOne(query)
+            res.send(result)
+        })
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentItemCollection.insertOne(payment)
+            res.send(result)
+        })
         app.post('/users', async (req, res) => {
             const users = req.body;
             const result = await userCollection.insertOne(users)
@@ -74,6 +121,22 @@ async function run() {
             const email = req.params.email
             const query = { email: email };
             const result = await userCollection.findOne(query)
+            res.send(result)
+        })
+        app.post('/reviews', async (req, res) => {
+            const reviews = req.body;
+            const result = await reviewCollection.insertOne(reviews)
+            res.send(result)
+        })
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const result = await reviewCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/reviews/:email', async (req, res) => {
+            const data = req.params.email
+            const query = { email: data };
+            const result = await reviewCollection.find(query).toArray()
             res.send(result)
         })
     }
